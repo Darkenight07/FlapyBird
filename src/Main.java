6f3fb1ed70ae080752b9bb43428aa539d7810e9d
Darkenight07 <samueldynamo11@gmail.com>
2023-11-05 22:20:35 +0100

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    public static void main(String  [] args) {
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setResizable(false);
+        frame.setLayout(new BorderLayout());
+
+        JuegoPanel panel = new JuegoPanel();
+        frame.add(panel, BorderLayout.CENTER);
+        panel.requestFocusInWindow();
+
+        // Siempre activo, es un bucle
+        frame.setVisible(true);
     }
 }
