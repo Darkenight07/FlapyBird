4aa3d77bdb9981275fa74765d2cf81150c243bf2
Darkenight07 <samueldynamo11@gmail.com>
2023-11-05 22:22:27 +0100

    Logica del juego

---
+++ src/Pajaro.java
@@ -0,0 +1,46 @@
+public class Pajaro {
+    // Posicion del pajaro
+    int x;
+    int y;
+    // Movimiento del pajaro
+    int velocidad;
+    int gravedad;
+
+    public Pajaro(int x, int y, int velocidad, int gravedad) {
+        this.x = x;
+        this.y = y;
+        this.velocidad = velocidad;
+        this.gravedad = gravedad;
+    }
+
+    public void saltar() {
+        int saltoMaximo = 10;
+        int contadorSalto = 0;
+
+        while (contadorSalto < saltoMaximo) {
+            y -= 1;
+            contadorSalto++;
+            if (y < 0) {
+                System.out.println("Se acabo la partida");
+            } else {
+                System.out.println("Salto: " + y + " " + contadorSalto);
+            }
+        }
+    }
+    public void bajar() {
+        y += velocidad;
+        if (y > 400) {
+            System.out.println("Se acabo la partida");
+        } else {
+            System.out.println("Bajada: " + y + " Velocidad: " + velocidad);
+        }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
