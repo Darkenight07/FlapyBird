d0fe873a5bf9876bad05507bb6519b16ee27b61d
Darkenight07 <66468335+Darkenight07@users.noreply.github.com>
2023-11-10 18:36:47 +0200

    Quitando warnings

--- src/TuberiaAbajo.java
+++ src/TuberiaAbajo.java
@@ -2,36 +2,29 @@ import java.util.Random;
 public class TuberiaAbajo {
     private int x;
     private int y;
-    private int velocidad;
+    private final int VELOCIDAD;
 
-    public TuberiaAbajo(int x, int y, int velocidad) {
+    public TuberiaAbajo(int x, int y, int VELOCIDAD) {
         this.x = x;
         this.y = y;
-        this.velocidad = velocidad;
+        this.VELOCIDAD = VELOCIDAD;
     }
 
     public void movimientoX() {
-        x -= velocidad;
+        x -= VELOCIDAD;
         // System.out.println("Tuberia Abajo X: " + x);
     }
 
     public int poscionAleatoriaY() {
         Random numeroAleatorio = new Random();
         int posicion = numeroAleatorio.nextInt(100) + 300; // Numero aleatorio de 300 a 401 para la posicion Y de la tuberia
+        System.out.println("Tuberia Abajo Y " + posicion);
         return posicion;
     }
 
-
-    public int setX(int x) {
-        return this.x = x;
-    }
+    public int setX(int x) {return this.x = x;}
     public int setY(int y) {return this.y = y; }
 
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
+    public int getX() {return x;}
+    public int getY() {return y;}
 }
