f0b471b8640a319e444d23ffc89ed2626f2f7803
Darkenight07 <66468335+Darkenight07@users.noreply.github.com>
2023-11-24 21:40:35 +0100

    Add files via upload

---
+++ src/Utilidades.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+public class Utilidades {
+    public static int generarNumeroAleatorio(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
+    }
+
+}
